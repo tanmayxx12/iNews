7827687c7e2976aba9a257e39b428b9a611b2107
tanmayxx12 <troy12rollsza@gmail.com>
2025-02-13 14:07:29 +0530

    Initial Commit

---
+++ iNews/iNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNewsApp.swift
+//  iNews
+//
+//  Created by Tanmay . on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct iNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
